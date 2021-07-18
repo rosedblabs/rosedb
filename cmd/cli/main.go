482840d2d67a484c9c12ef16dc4512757de9a7c6
liukehao7 <liukehao@uniontech.com>
2021-07-18 19:03:20 +0800

    fix: Solve the problem of losing connection with the server and not exiting
    After detecting a loss of connection with the server,
    the client should exit

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -169,8 +169,8 @@ func main() {
 			command, args := parseCommandLine(cmd)
 			rawResp, err := conn.Do(command, args...)
 			if err != nil {
-				fmt.Printf("(error) %v \n", err)
-				continue
+				fmt.Printf("(Lost connection with server) %v \n", err)
+				break
 			}
 			switch reply := rawResp.(type) {
 			case []byte:
