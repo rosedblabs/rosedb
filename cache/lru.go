a5084e8df4d56128bb1f9323bf80d9384dbed526
rglkt <835993649@qq.com>
2021-07-29 21:12:58 +0800

    add annotation for cache

--- cache/lru.go
+++ cache/lru.go
@@ -12,8 +12,8 @@ var KeyNotFoundError = errors.New("Key not found")
 type LRUCache struct {
 	size      int
 	mu        sync.RWMutex
-	items     map[interface{}]*list.Element
-	evictList *list.List
+	items     map[interface{}]*list.Element //store data
+	evictList *list.List                    //store priority for LRU
 }
 type lruItem struct {
 	key        interface{}
@@ -21,25 +21,29 @@ type lruItem struct {
 	expiration *time.Time
 }
 
+//NewLRUCache create a LRUCache with size
 func NewLRUCache(size int) *LRUCache {
 	if !(size > 0) {
 		panic("Cache size should bigger than 0")
 	}
 	c := &LRUCache{}
 	c.Setsize(size)
-	c.init()
+	c.initLRUCache()
 	return c
 }
 
-func (c *LRUCache) init() {
+//initLRUCache init LRUCache.evictList and LRUCache.items
+func (c *LRUCache) initLRUCache() {
 	c.evictList = list.New()
 	c.items = make(map[interface{}]*list.Element, c.size+1)
 }
 
+//Setsize set size
 func (c *LRUCache) Setsize(size int) {
 	c.size = size
 }
 
+//set set key-value in cache
 func (c *LRUCache) set(key, value interface{}) interface{} {
 	var item *lruItem
 	//if exists,update the value and bring it to front
@@ -61,13 +65,14 @@ func (c *LRUCache) set(key, value interface{}) interface{} {
 	return item
 }
 
-// set a new key-value pair
+// Set use lock to set key-value
 func (c *LRUCache) Set(key, value interface{}) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.set(key, value)
 }
 
+//SetWithExpire set key-value with expire time
 func (c *LRUCache) SetWithExpire(key, value interface{}, expiration time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -76,6 +81,8 @@ func (c *LRUCache) SetWithExpire(key, value interface{}, expiration time.Duratio
 	it.(*lruItem).expiration = &t
 }
 
+//get get value by key and update priority
+//return KeyNotFoundError when fail to find
 func (c *LRUCache) get(key interface{}) (interface{}, error) {
 	item, ok := c.items[key]
 	if ok {
@@ -89,6 +96,8 @@ func (c *LRUCache) get(key interface{}) (interface{}, error) {
 	}
 	return nil, KeyNotFoundError
 }
+
+//Get use lock to get value
 func (c *LRUCache) Get(key interface{}) (interface{}, error) {
 	// get value will change priority of element
 	// so need to use Lock() instead of RLock()
@@ -97,6 +106,7 @@ func (c *LRUCache) Get(key interface{}) (interface{}, error) {
 	return c.get(key)
 }
 
+//has find key if exists
 func (c *LRUCache) has(key interface{}) bool {
 	item, ok := c.items[key]
 	if !ok {
@@ -104,12 +114,16 @@ func (c *LRUCache) has(key interface{}) bool {
 	}
 	return !item.Value.(*lruItem).IsExpired()
 }
+
+//Has find key if exists and quicker than get because of use RLock
 func (c *LRUCache) Has(key interface{}) bool {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 	return c.has(key)
 }
 
+//remove remove key-value in cache
+//success will return true
 func (c *LRUCache) remove(key interface{}) bool {
 	if ent, ok := c.items[key]; ok {
 		c.removeElement(ent)
@@ -117,23 +131,28 @@ func (c *LRUCache) remove(key interface{}) bool {
 	}
 	return false
 }
+
+//Remove use lock to remove key-value
 func (c *LRUCache) Remove(key interface{}) bool {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	return c.remove(key)
 }
 
+//Clear remove all key-value in cache
 func (c *LRUCache) Clear() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.init()
+	c.initLRUCache()
 }
 
+//evict remove the oldest key-value
 func (c *LRUCache) evict() {
 	ent := c.evictList.Back()
 	c.removeElement(ent)
 }
 
+//removeElement remove some Element
 func (c *LRUCache) removeElement(e *list.Element) {
 	c.evictList.Remove(e)
 	entry := e.Value.(*lruItem)
