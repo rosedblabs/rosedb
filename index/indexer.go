e9e779c035a4b72bf9f60d3e624cf639b518a8ab
eddieVim <eddievim@foxmail.com>
2021-10-25 16:58:10 +0800

    Update indexer annotation, indexer is a component of data types index.

--- index/indexer.go
+++ index/indexer.go
@@ -4,7 +4,7 @@ import (
 	"github.com/roseduan/rosedb/storage"
 )
 
-// Indexer the data index info, stored in skip list.
+// Indexer the data index info, a component of data types index.
 type Indexer struct {
 	Meta   *storage.Meta // metadata info.
 	FileId uint32        // the file id of storing the data.
