cd6eebf2f6962d757ee787e4177bca304da31190
roseduan <roseduan520@gmail.com>
2022-04-09 21:39:43 +0800

    fix sets

---
+++ sets_test.go
@@ -0,0 +1,28 @@
+package rosedb
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+func TestRoseDB_SAdd(t *testing.T) {
+	path := filepath.Join("/tmp", "rosedb")
+	opts := DefaultOptions(path)
+	db, err := Open(opts)
+	//defer destroyDB(db)
+	if err != nil {
+		t.Error("open db err ", err)
+	}
+
+	db.SAdd(GetKey(12), GetValue16B())
+	db.SAdd(GetKey(13), GetValue16B())
+	db.SAdd(GetKey(12), GetValue16B())
+
+	members, err := db.SMembers(GetKey(12))
+	t.Log(err)
+	for _, m := range members {
+		t.Log(string(m))
+	}
+
+	db.SRem(GetKey(12))
+}
