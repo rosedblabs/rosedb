e1756238894f210a6138119c205aa06e7d26c334
roseduan <roseduan520@gmail.com>
2022-05-22 17:47:54 +0800

    fix eg

--- examples/open/open_db.go
+++ examples/open/open_db.go
@@ -8,11 +8,13 @@ import (
 
 func main() {
 	path := filepath.Join("/tmp", "rosedb")
+	// specify other options
+	// opts.XXX
 	opts := rosedb.DefaultOptions(path)
 	db, err := rosedb.Open(opts)
 	if err != nil {
 		fmt.Printf("open rosedb err: %v", err)
 		return
 	}
-	defer db.Close()
+	_ = db.Close()
 }
