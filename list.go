8c0e00949dfa145f40f1304f8226a1fdacf7ebc0
Reid00 <isreid.zhang@outlook.com>
2022-05-23 16:43:50 +0800

    update conconvertLogicalIndexToSeq func comment

--- list.go
+++ list.go
@@ -203,7 +203,7 @@ func (db *RoseDB) LRange(key []byte, start, end int) (values [][]byte, err error
 		return nil, ErrIndexStartLagerThanEnd
 	}
 
-	// the endSeq value is including
+	// the endSeq value is included
 	for seq := startSeq; seq < endSeq+1; seq++ {
 		encKey := db.encodeListKey(key, seq)
 		val, err := db.getVal(encKey, List)
@@ -217,7 +217,7 @@ func (db *RoseDB) LRange(key []byte, start, end int) (values [][]byte, err error
 }
 
 // convertLogicalIndexToSeq just convert logical index to physical seq
-// whether physical seq legal or not, just convert
+// whether physical seq is legal or not, just convert
 func (db *RoseDB) convertLogicalIndexToSeq(headSeq, tailSeq uint32, index int) (uint32, error) {
 	var seq uint32
 
