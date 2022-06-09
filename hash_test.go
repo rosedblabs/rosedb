1d03432c46a5036c8cbdd09a601d8bd554a4ca49
Reid00 <isreid.zhang@outlook.com>
2022-06-09 13:58:59 +0800

    refactor HSet to HMSet function

--- hash_test.go
+++ hash_test.go
@@ -4,7 +4,7 @@ import (
 	"math"
 	"path/filepath"
 	"testing"
-
+	"errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,7 +12,7 @@ func TestRoseDB_HSet(t *testing.T) {
 	t.Run("fileio", func(t *testing.T) {
 		testRoseDBHSet(t, FileIO, KeyOnlyMemMode)
 	})
-	t.Run("fileio", func(t *testing.T) {
+	t.Run("mmap", func(t *testing.T) {
 		testRoseDBHSet(t, MMap, KeyValueMemMode)
 	})
 }
@@ -27,9 +27,8 @@ func testRoseDBHSet(t *testing.T, ioType IOType, mode DataIndexMode) {
 	defer destroyDB(db)
 
 	type args struct {
-		key   []byte
-		field []byte
-		value []byte
+		key []byte
+		arg [][]byte
 	}
 	tests := []struct {
 		name    string
@@ -38,22 +37,49 @@ func testRoseDBHSet(t *testing.T, ioType IOType, mode DataIndexMode) {
 		wantErr bool
 	}{
 		{
-			"nil", db, args{key: nil, field: nil, value: GetKey(123)}, false,
+			"nil-key-and-field", db, args{key: nil, arg: [][]byte{nil, []byte("val-0")}}, false,
 		},
 		{
-			"nil-value", db, args{key: GetKey(1), field: GetKey(11), value: nil}, false,
+			"wrong-num-of-args", db, args{key: GetKey(2), arg: [][]byte{[]byte("field-0")}}, true,
 		},
 		{
-			"normal", db, args{key: GetKey(1), field: GetKey(11), value: GetValue16B()}, false,
+			"normal-single-pair", db, args{key: GetKey(3), arg: [][]byte{[]byte("field-0"), []byte("val-0")}}, false,
+		},
+		{
+			"normal-mulit-pair", db, args{key: GetKey(4), arg: [][]byte{[]byte("field-0"), []byte("val-0"),
+				[]byte("field-1"), []byte("val-1")}}, false,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.db.HSet(tt.args.key, tt.args.field, tt.args.value); (err != nil) != tt.wantErr {
+			err := tt.db.HSet(tt.args.key, tt.args.arg...)
+			if (err != nil) != tt.wantErr {
 				t.Errorf("HSet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr && !errors.Is(err, ErrWrongNumberOfArgs) {
+				t.Errorf("HSet() error = %v, expected error = %v", err, ErrWrongNumberOfArgs)
 			}
 		})
 	}
+
+	t.Run("check-nil-key-and-field", func(t *testing.T) {
+		val, err := db.HGet(nil, nil)
+		assert.Nil(t, err)
+		assert.Equal(t, []byte(nil), val)
+	})
+
+	t.Run("check-single-field", func(t *testing.T) {
+		val, err := db.HGet(GetKey(3), []byte("field-0"))
+		assert.Nil(t, err)
+		assert.Equal(t, []byte("val-0"), val, "single field not same")
+	})
+
+	t.Run("check-mulit-field", func(t *testing.T) {
+		value, err := db.HMGet(GetKey(4), []byte("field-0"), []byte("field-1"))
+		assert.Nil(t, err)
+		assert.Equal(t, [][]byte{[]byte("val-0"), []byte("val-1")}, value, "multi field not same")
+	})
 }
 
 func TestRoseDB_HSetNX(t *testing.T) {
