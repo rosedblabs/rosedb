b8c3291c67341f2822c50ce574ef0d045633a4c9
roseduan <roseduan520@gmail.com>
2023-06-21 14:47:37 +0800

    add tests on windows (#221)
    * add tests on windows
    
    * add vet and fmt

--- utils/file.go
+++ utils/file.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"io/fs"
 	"path/filepath"
-	"syscall"
 )
 
 // DirSize get directory size
@@ -19,17 +18,4 @@ func DirSize(dirPath string) (int64, error) {
 		return nil
 	})
 	return size, err
-}
-
-// AvailableDiskSize get available disk size
-func AvailableDiskSize() (uint64, error) {
-	wd, err := syscall.Getwd()
-	if err != nil {
-		return 0, err
-	}
-	var stat syscall.Statfs_t
-	if err = syscall.Statfs(wd, &stat); err != nil {
-		return 0, err
-	}
-	return stat.Bavail * uint64(stat.Bsize), nil
 }
