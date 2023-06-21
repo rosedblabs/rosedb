db3b86250db94cac986a2a8e2e7d72bcfe710b01
roseduan <roseduan7@163.com>
2023-06-21 16:42:31 +0800

    fix create dir mode

--- utils/rand_kv.go
+++ utils/rand_kv.go
@@ -3,10 +3,12 @@ package utils
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 var (
+	lock    = sync.Mutex{}
 	randStr = rand.New(rand.NewSource(time.Now().Unix()))
 	letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 )
@@ -20,7 +22,9 @@ func GetTestKey(i int) []byte {
 func RandomValue(n int) []byte {
 	b := make([]byte, n)
 	for i := range b {
+		lock.Lock()
 		b[i] = letters[randStr.Intn(len(letters))]
+		lock.Unlock()
 	}
 	return []byte("rosedb-test-value-" + string(b))
 }
