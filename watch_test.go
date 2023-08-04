599fe68ddce97220cb50e125e5a9302ad30615a7
Jeremy <acm_123@163.com>
2023-08-04 20:02:49 +0800

    add unit test

--- watch_test.go
+++ watch_test.go
@@ -69,16 +69,68 @@ func TestWatch_Put_Watch(t *testing.T) {
 
 	w, err := db.Watch()
 	assert.Nil(t, err)
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 50; i++ {
 		key := utils.GetTestKey(rand.Int())
 		value := utils.RandomValue(128)
 		err = db.Put(key, value)
 		assert.Nil(t, err)
-		select {
-		case event := <-w:
-			assert.Equal(t, ActionPut, event.Action)
-			assert.Equal(t, key, event.Key)
+		event := <-w
+		assert.Equal(t, ActionPut, event.Action)
+		assert.Equal(t, key, event.Key)
+		assert.Equal(t, value, event.Value)
+	}
+}
+
+func TestWatch_Put_Delete_Watch(t *testing.T) {
+	options := DefaultOptions
+	options.Watchable = true
+	db, err := Open(options)
+	assert.Nil(t, err)
+	defer destroyDB(db)
+
+	w, err := db.Watch()
+	assert.Nil(t, err)
+
+	key := utils.GetTestKey(rand.Int())
+	value := utils.RandomValue(128)
+	db.Put(key, value)
+	db.Delete(key)
+
+	for i := 0; i < 2; i++ {
+		event := <-w
+		assert.Equal(t, key, event.Key)
+		if event.Action == ActionPut {
 			assert.Equal(t, value, event.Value)
+		} else if event.Action == ActionDelete {
+			assert.Equal(t, 0, len(event.Value))
 		}
+	}
+}
+
+func TestWatch_Batch_Put_Watch(t *testing.T) {
+	options := DefaultOptions
+	options.Watchable = true
+	db, err := Open(options)
+	assert.Nil(t, err)
+	defer destroyDB(db)
+
+	w, err := db.Watch()
+	assert.Nil(t, err)
+
+	times := 100
+	batch := db.NewBatch(DefaultBatchOptions)
+	for i := 0; i < times; i++ {
+		err = batch.Put(utils.GetTestKey(rand.Int()), utils.RandomValue(128))
+		assert.Nil(t, err)
+	}
+	batch.Commit()
+
+	var batchId uint64
+	for i := 0; i < times; i++ {
+		event := <-w
+		if i == 0 {
+			batchId = event.BatchId
+		}
+		assert.Equal(t, batchId, event.BatchId)
 	}
 }
